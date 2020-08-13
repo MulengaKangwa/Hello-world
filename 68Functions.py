585bdd6b5fc80add8eca7050f30bd61bcf26d5ac
MulengaKangwa <67792390+MulengaKangwa@users.noreply.github.com>
2020-08-13 11:28:40 +0100

    Update 68Functions.py

--- 68Functions.py
+++ 68Functions.py
@@ -3,6 +3,6 @@
 # These functions can be inbuilt or can be specified by the coder.
 # The advantages of functions: (1) re-usable (2) modularity (3) maintenance
 
-def average(a,b):
-    print("The average of two numbers:",(a/b)/2)
-average(10,20)
+def average(a,b,c):
+    print("The average of two numbers:",(a/b*c)/2)
+average(10,20,5)
